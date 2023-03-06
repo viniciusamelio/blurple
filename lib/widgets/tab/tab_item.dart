d3dbde4be557327ae3cb8e2e357ed43078fea7dd
Vinicius Amélio <vinicius.amelio@cubos.io>
2023-03-06 20:52:39 -0300

    feature: default tab item

---
+++ lib/widgets/tab/tab_item.dart
@@ -0,0 +1,51 @@
+import 'package:blurple/themes/theme_data.dart';
+import 'package:blurple/tokens/size_tokens.dart';
+import 'package:flutter/widgets.dart';
+
+class DefaultTabItem extends StatelessWidget {
+  const DefaultTabItem({
+    Key? key,
+    required this.child,
+    required this.isActive,
+    this.activeAccentColor,
+    this.inactiveChild,
+  }) : super(key: key);
+
+  final Widget child;
+  final bool isActive;
+  final Color? activeAccentColor;
+  final Widget? inactiveChild;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = BlurpleThemeData.of(context);
+
+    return Padding(
+      padding: theme.spacingScheme.inputPadding,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Visibility(
+            visible: isActive,
+            replacement: inactiveChild ?? child,
+            child: child,
+          ),
+          SizedBox(
+            height: SizeTokens.xs,
+          ),
+          Visibility(
+            visible: isActive,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.circular(theme.radiusScheme.elevatedRadius),
+                color: activeAccentColor ?? theme.colorScheme.accentColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
