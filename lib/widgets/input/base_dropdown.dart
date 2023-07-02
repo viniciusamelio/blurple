8662951c9536524855b31ea338f132ae1af55752
Vinicius Amélio <vinicius.amelio@zoho.com>
2023-07-02 09:28:52 -0300

    feature: searchable dropdown

---
+++ lib/widgets/input/base_dropdown.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+
+import '../../themes/theme_data.dart';
+
+class BaseSearchableDropdown<T> extends StatelessWidget {
+  const BaseSearchableDropdown({
+    super.key,
+    this.hint,
+    this.direction,
+    required this.itemBuilder,
+    required this.onSuggestionSelected,
+    required this.suggestionsCallback,
+  });
+
+  final String? hint;
+  final AxisDirection? direction;
+  final Widget Function(BuildContext context, T value) itemBuilder;
+  final void Function(T value) onSuggestionSelected;
+  final FutureOr<Iterable<T>> Function(String pattern) suggestionsCallback;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = BlurpleThemeData.of(context);
+
+    return TypeAheadField<T>(
+      suggestionsCallback: suggestionsCallback,
+      direction: direction ?? AxisDirection.down,
+      textFieldConfiguration: TextFieldConfiguration(
+        decoration: InputDecoration(
+          hintText: hint,
+          filled: true,
+          contentPadding: theme.spacingScheme.inputPadding,
+          labelStyle: theme.fontScheme.input,
+          hintStyle: theme.fontScheme.input,
+          fillColor: theme.colorScheme.inputBackgroundColor,
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              width: .1,
+              color: theme.colorScheme.borderColor,
+            ),
+            borderRadius: BorderRadius.circular(
+              theme.radiusScheme.inputRadius,
+            ),
+          ),
+        ),
+      ),
+      hideSuggestionsOnKeyboardHide: true,
+      itemBuilder: itemBuilder,
+      onSuggestionSelected: onSuggestionSelected,
+    );
+  }
+}
